4e4fb1963c59891f71e6e2906155a0f2be2d4316
Ebenezr <ebenezarbukosia@gmail.com>
2023-05-21 00:18:31 +0300

    [ADD] aside bar UI

--- src/Components/aside.jsx
+++ src/Components/aside.jsx
@@ -1,4 +1,4 @@
-import { Sidebar } from "flowbite-react";
+import { Avatar, Button, Sidebar } from "flowbite-react";
 import React from "react";
 import { Link } from "react-router-dom";
 import stud from "./svg/stud";
@@ -10,11 +10,16 @@ import reporticon from "./svg/reporticon";
 import payicon from "./svg/payicon";
 import classicon from "./svg/classicon";
 import teachericon from "./svg/teachericon";
-
+import { CiLogout } from "react-icons/ci";
+import { useNavigate } from "react-router-dom";
 const Aside = () => {
+  const navigate = useNavigate();
   return (
-    <div className="w-fit">
+    <div className="w-fit h-screen flex flex-col">
       <Sidebar aria-label="Sidebar Menu">
+        <Sidebar.Logo href="#" img="favicon.png" imgAlt="">
+          School Soft
+        </Sidebar.Logo>
         <Sidebar.Items>
           <Sidebar.ItemGroup>
             <Sidebar.Item as={Link} to="dashboard" icon={dash}>
@@ -48,6 +53,30 @@ const Aside = () => {
           </Sidebar.ItemGroup>
         </Sidebar.Items>
       </Sidebar>
+      <div className="flex flex-col justify-center w-full px-2 py-4">
+        <div className="flex items-end  pb-4 w-full ">
+          <Avatar placeholderInitials="EB" rounded={true}>
+            <div className="space-y-1 font-medium dark:text-white">
+              <div>Jese Leos</div>
+              <div className="text-sm text-gray-500 dark:text-gray-400">
+                ADMIN
+              </div>
+            </div>
+          </Avatar>
+        </div>
+        <div>
+          <Button
+            className="mx-auto w-full"
+            color="purple"
+            onClick={() => {
+              navigate("/");
+            }}
+          >
+            Sign out
+            <CiLogout className="ml-2 h-5 w-5" />
+          </Button>
+        </div>
+      </div>
     </div>
   );
 };
