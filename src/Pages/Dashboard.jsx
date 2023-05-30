c43c2f9b89159eac70a99c3982e0f3f46702416b
Ebenezr <ebenezarbukosia@gmail.com>
2023-05-30 22:36:14 +0300

    minor fixes

--- src/Pages/Dashboard.jsx
+++ src/Pages/Dashboard.jsx
@@ -183,8 +183,8 @@ function SkeletonLoader() {
     <div className="grid grid-cols-2 bg-white shadow-md animate-pulse w-full h-28 rounded-lg">
       <div className="place-items-center grid mx-auto my-auto rounded-full bg-slate-200 animate-pulse h-24 w-24 "></div>
       <div className="flex flex-col my-auto px-2 gap-2  ">
-        <div className="bg-gray-200 animate-pulse h-12 rounded"></div>
-        <div className="bg-gray-200 animate-pulse h-6 rounded"></div>
+        <div className="bg-slate-200 animate-pulse h-12 w-20 rounded"></div>
+        <div className="bg-slate-200 animate-pulse h-6 w-20 rounded"></div>
       </div>
     </div>
   );
